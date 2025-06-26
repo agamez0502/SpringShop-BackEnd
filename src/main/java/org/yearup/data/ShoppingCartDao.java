cedad82a616689e319af58e1c161eb7a02e937a0
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-26 14:21:37 -0500

    Added: Completed ShoppingCartDao. Added add, update, and clear methods to the dao. Started working on ShoppingCartController.

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -5,5 +5,9 @@ import org.yearup.models.ShoppingCart;
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
+
     // add additional method signatures here
+    void add(int userId, int productId);
+    void update(int userId, int productId, int quantity);
+    void clear(int userId);
 }
