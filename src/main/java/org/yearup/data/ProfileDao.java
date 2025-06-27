7d5460bf473ead0b223baf8d4905f6dcc6f280d2
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-27 07:11:32 -0500

    Added: Completed ProfileDao. I just updated it and added the getByUserId and update methods.

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,14 @@
 package org.yearup.data;
 
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.yearup.models.Profile;
+
+import java.security.Principal;
 
 public interface ProfileDao
 {
     Profile create(Profile profile);
+    Profile getByUserId(int userId);
+    void update(int userId, Profile profile);
 }
